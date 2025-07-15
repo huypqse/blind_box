f67fad7ceffa7aab945af91a65707a64aa677a36
huypqse <huypqse@gmail.com>
2025-07-16 00:48:23 +0700

    feat: init project

---
+++ src/main/java/com/example/Blind_Box/BlindBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.Blind_Box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Blind_Box/BlindBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blind_Box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
